3d9be9ebdd036e720103f365f3ca5162d723d431
saumya200 <saumyaas2000@yahoo.com>
2023-11-07 12:45:02 +0530

    new changes

--- moduleTUT.js
+++ moduleTUT.js
@@ -6,20 +6,31 @@
 
 //-------------------------------------------------------------------------------------------------------------------------
 //-------------------------Built-in Modules are of 4 types:
-//1)OS Module , 2)PATH Module 3)FS Module, 4)HTTP Module
-//-------------------------------------------------------------------------------------------------------------------------
-//OS MODULE //
-const os = require('os');
+//1)OS Module , 
+//2)PATH Module 
+//3)FS Module, 
+//4)HTTP Module-------------------------------------------------------------------------------------------
 
-const systemUpTime = os.uptime();
+//----------->OS MODULE <---------------------------//
 
-const userInfo = os.userInfo();
+//import for OS using the require() method;
+const os = require('os'); 
+
+//tells the system uptime in seconds
+const systemUpTime = os.uptime(); 
+
+//gives the information about the current user
+const userInfo = os.userInfo(); 
 
 const otherInfo = {
-    name : os.type(),
-    release : os.release(),
-    totalMem : os.type(),
-    freeMem : os.freemem()
+  //Tells the name of the Operating System
+    name : os.type(), 
+    //Tells the release version of the Operating System
+    release : os.release(), 
+     //Tells the total amount of memory available in bytes
+     totalMem : os.type(),
+   // Tells the total amount of free memory available in 
+    freeMem : os.freemem() 
 }
 
 console.log(systemUpTime);
@@ -27,10 +38,13 @@ console.log(userInfo);
 console.log(otherInfo);
 
 //-----------------------------------------------------------------------------------------------------------------------------
-//PATH MODULE//
-const path = require('path');
+//-------------------->PATH MODULE<-------------------------------------//
 
-const myPath = '/home/saumya/REACT/nodeJsTut/app.js'
+//import for PATH using the require() method
+const path = require('path'); 
+
+//// Assigning a path to the myPath variable
+const myPath = '/home/saumya/REACT/nodeJsTut/app.js' 
 
 const pathInfo = {
     fileName : path.basename(myPath),
@@ -44,7 +58,10 @@ const pathInfo = {
 console.log(myPath);
 console.log(pathInfo);
 console.log(path.sep);
+
+//accepts path as strings
 console.log(path.join('grandParentFolder', 'parentFolder', 'child.txt'))
+ 
 console.log(path.resolve('grandParentFolder', 'parentFolder', 'child.txt'))
 
 //-------------------------------------------------------------------------------------------------------------------------------
