7a820a1f950c4e2e2cd8d61386e151097d7baaeb
saumya200 <saumyaas2000@yahoo.com>
2023-11-07 08:04:43 +0530

    nodeJs tutorial

---
+++ app.js
@@ -0,0 +1,44 @@
+// const hello = require('./hello.js')
+
+// console.log(hello.foo);
+// hello.sayHello1();
+// hello.sayHello2();
+
+//OS MODULE //
+const os = require('os');
+
+const systemUpTime = os.uptime();
+
+const userInfo = os.userInfo();
+
+const otherInfo = {
+    name : os.type(),
+    release : os.release(),
+    totalMem : os.type(),
+    freeMem : os.freemem()
+}
+
+console.log(systemUpTime);
+console.log(userInfo);
+console.log(otherInfo);
+
+
+//PATH MODULE//
+const path = require('path');
+
+const myPath = '/home/saumya/REACT/nodeJsTut/app.js'
+
+const pathInfo = {
+    fileName : path.basename(myPath),
+    folderName : path.dirname(myPath),
+    extensionName : path.extname(myPath),
+    absoluteOrNot : path.isAbsolute(myPath),
+    detailInfo : path.parse(myPath),
+    
+}
+
+console.log(myPath);
+console.log(pathInfo);
+console.log(path.sep);
+console.log(path.join('grandParentFolder', 'parentFolder', 'child.txt'))
+console.log(path.resolve('grandParentFolder', 'parentFolder', 'child.txt'))
---
+++ hello.js
@@ -0,0 +1,13 @@
+// function sayHello1(){
+//     console.log("Hello1")
+// }
+
+// function sayHello2(){
+//     console.log("Hello2")
+// }
+
+// module.exports = {
+//     foo : 'id',
+//     sayHello1 : sayHello1,
+//     sayHello2 : sayHello2
+// };
