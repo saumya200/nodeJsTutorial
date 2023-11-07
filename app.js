c54f4d01618f26bde2f0b95e607f5f84e8ca4a1b
saumya200 <saumyaas2000@yahoo.com>
2023-11-07 08:32:33 +0530

    nodeJs tutorial

--- app.js
+++ app.js
@@ -5,40 +5,52 @@
 // hello.sayHello2();
 
 //OS MODULE //
-const os = require('os');
+// const os = require('os');
 
-const systemUpTime = os.uptime();
+// const systemUpTime = os.uptime();
 
-const userInfo = os.userInfo();
+// const userInfo = os.userInfo();
 
-const otherInfo = {
-    name : os.type(),
-    release : os.release(),
-    totalMem : os.type(),
-    freeMem : os.freemem()
-}
+// const otherInfo = {
+//     name : os.type(),
+//     release : os.release(),
+//     totalMem : os.type(),
+//     freeMem : os.freemem()
+// }
 
-console.log(systemUpTime);
-console.log(userInfo);
-console.log(otherInfo);
+// console.log(systemUpTime);
+// console.log(userInfo);
+// console.log(otherInfo);
 
 
 //PATH MODULE//
-const path = require('path');
+// const path = require('path');
 
-const myPath = '/home/saumya/REACT/nodeJsTut/app.js'
+// const myPath = '/home/saumya/REACT/nodeJsTut/app.js'
 
-const pathInfo = {
-    fileName : path.basename(myPath),
-    folderName : path.dirname(myPath),
-    extensionName : path.extname(myPath),
-    absoluteOrNot : path.isAbsolute(myPath),
-    detailInfo : path.parse(myPath),
+// const pathInfo = {
+//     fileName : path.basename(myPath),
+//     folderName : path.dirname(myPath),
+//     extensionName : path.extname(myPath),
+//     absoluteOrNot : path.isAbsolute(myPath),
+//     detailInfo : path.parse(myPath),
     
-}
+// }
 
-console.log(myPath);
-console.log(pathInfo);
-console.log(path.sep);
-console.log(path.join('grandParentFolder', 'parentFolder', 'child.txt'))
-console.log(path.resolve('grandParentFolder', 'parentFolder', 'child.txt'))
+// console.log(myPath);
+// console.log(pathInfo);
+// console.log(path.sep);
+// console.log(path.join('grandParentFolder', 'parentFolder', 'child.txt'))
+// console.log(path.resolve('grandParentFolder', 'parentFolder', 'child.txt'))
+
+//FS MODULE//
+const fs = require('fs');
+
+fs.mkdir('./myFolder',(err) => {
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("Folder created successfully !")
+    }
+})
